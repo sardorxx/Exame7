a737cb8db1d49681577fe7754934f7b6ef197a02
Sardor Xalilov <138990743+sardorxx@users.noreply.github.com>
2024-01-27 22:00:59 +0500

    first commit

--- app/api/v1/urls.py
+++ app/api/v1/urls.py
@@ -7,8 +7,9 @@ router.register(r'name_and_app_type', UserPassManagerlFilterViewSet)
 urlpatterns = [
     path('hello_world/', Hello_World, name='hello_world'),
     path('password_type/', password_type, name='password_type'),
-    path('user/login/', UserLoginView.as_view(), name='user'),
-]
+    path('login/', login_view, name='login'),
+    path('signup/', signup_view, name='signup'),
+    path('logout/', logout_view, name='logout'),]
 
 
 urlpatterns += router.urls
